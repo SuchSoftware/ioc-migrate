b36e2fb2d15c826247c21959ddade25986c48e70
Ethan Garofolo <egarofolo@ldschurch.org>
2015-07-27 11:57:02 -0600

    Creates migration files

---
+++ test/generatorTests.js
@@ -0,0 +1,30 @@
+'use strict'
+
+var fakeFs = {
+  writeFile: function writeFile(filename, data, cb) {
+    process.nextTick(cb) 
+  }
+}
+
+var generator = require('../lib/generator')
+
+require('./testHelper')(function(assert) {
+  describe('Generator:', function () {
+    it('should generate the correct filename', function(done) {
+      var now = new Date('2012-01-01 17:00:00')  
+  
+      var migrateConfig = {
+        migrationsPath: 'foo'
+      }
+  
+      function noop() {}
+  
+      generator.generate(migrateConfig, 'testMigration', noop, fakeFs, now, function(err, filename) {
+        assert.ifError(err) 
+  
+        assert.equal(filename, 'foo/2012-01-01_17-00-00-testMigration.js')
+        done()
+      })
+    })
+  })
+})
---
+++ test/testHelper.js
@@ -0,0 +1,9 @@
+'use strict'
+
+module.exports = w
+
+var assert = require('assert')
+
+function w(test, name) {
+  test(assert)
+}
